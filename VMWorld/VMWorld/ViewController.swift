54537885fb56a6807613e055de0e02dfb500fe01
xeliot <daveho8888@gmail.com>
2018-07-24 20:07:14 -0700

    init commit

---
+++ VMWorld/VMWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VMWorld
+//
+//  Created by Dave Ho on 7/24/18.
+//  Copyright © 2018 Dave Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
